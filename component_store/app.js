5e73976733c105bb2ac8edb969af9c38547702c1
Nisha Shukla <nisha.shukla@inspired.consulting>
2022-12-09 16:59:55 +0100

    added handlebars as template engine

--- component_store/app.js
+++ component_store/app.js
@@ -1,20 +1,33 @@
 const express = require('express');
+const expressHandlebars = require('express-handlebars');
 const app = express();
+var hbs = expressHandlebars.create({});
+var path = require('path');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 app.use(cors());
-app.use(express.json())
-
 const port = process.env.PORT || '8383';
-app.use(express.static('views'))
+
+app.use(express.json())
+//console.log(path.join(__dirname, "./public"))
+app.use(express.static(path.join(__dirname, './public')));
+
+//app.use("/public", express.static('./public/'));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+const InfoRouter = require('./routes/info');
+const UploadsRouter = require('./routes/uploads');
 
 app.use(fileUpload());
-
-const InfoRouter = require('./src/routes/info');
-const UploadsRouter = require('./src/routes/uploads');
-
 app.use('/info', InfoRouter);
 app.use('/upload', UploadsRouter);
+
+app.get('/', (req, res) => {
+	    res.render("index", { 
+	    })
+	})
 
 app.listen(port, () => {
 	console.log(`Server listening on Port: ${port}`)
