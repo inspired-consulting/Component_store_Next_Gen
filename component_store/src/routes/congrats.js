16a0107dfc71a201eac5c5896ec26000a52e7acf
Fenja Pestke <fenja.pestke@gmail.com>
2023-01-04 18:36:48 +0100

    change congrats url  to name instead of id and add functionality for displaying oldversions of component

--- component_store/src/routes/congrats.js
+++ component_store/src/routes/congrats.js
@@ -1,29 +1,31 @@
 const express = require('express');
 const fs = require('fs');
+const { readFromDB } = require('../model/componentTest');
 const router = express.Router();
-//const files = fs.readdirSync( './uploads');
-const Version = require('../models/version');
+// const files = fs.readdirSync( './uploads');
+// const Version = require('../models/version');
 
-
-router.get('/:id', (req, res) => {
-    const componentId = req.params.id;
-        Version.getComponentNameAndVersionById(componentId)
+router.get('/:name', (req, res) => {
+    const componentName = req.params.name;
+    readFromDB(componentName)
         .then(rows => {
-            const component = rows.length > 0 ? rows[0] : false;
-            console.log("component", component);
-            res.render("congratulation", {
-                component: component,
+            const component = rows.length > 0 ? rows.pop() : false;
+            const olderversions = rows.length > 0 ? rows.reverse() : false;
+            console.log('component', component);
+            res.render('congratulation', {
+                component,
+                olderversions,
                 name: component.name,
                 version: component.version,
-                entryFile: component.entry_file,
+                information: component.readme,
+                entryFile: component.entry_file
             })
         })
-    .catch(err => {
-        if (err) return res.status(500).send(err);
-        return next(err);
-    })
+        .catch(err => {
+            if (err) return res.status(500).send(err);
+            return err;
+        })
 })
-
 
 // router.get('/:id', (req, res) => {
 //     const componentId = req.params.id;
@@ -50,49 +52,17 @@ router.get('/:id', (req, res) => {
 //     });
 // })
 
-async function readFileLocally() {
+async function readFileLocally () {
     try {
         const rawdata = await fs.promises.readFile('componentData.json');
         const jsonData = JSON.parse(rawdata)
-        return jsonData.component; 
+        return jsonData.component;
     } catch (err) {
-      console.error('Error occurred while reading file!', err);
+        console.error('Error occurred while reading file!', err);
     }
 }
 
-
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // router.get('/:id', (req, res) => {
 //     console.log("request",req.params.id);
@@ -108,4 +78,3 @@ module.exports = router;
 //         })
 //     });
 // })
-
