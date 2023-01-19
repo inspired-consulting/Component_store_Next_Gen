f448f715b7e24a48900afc08bf880ff7525533e0
Nisha Shukla <nisha.shukla@inspired.consulting>
2023-01-19 17:50:40 +0100

    disabled submit btn if input is empty

--- component_store/public/index.js
+++ component_store/public/index.js
@@ -1,4 +1,5 @@
 
+// summernote
 // eslint-disable-next-line no-undef
 $('#summernote').summernote({
     placeholder: 'Add some instructions to integrate the component.',
@@ -14,3 +15,71 @@ $('#summernote').summernote({
         ['view', ['fullscreen', 'codeview', 'help']]
     ]
 });
+
+// File upload
+const uploadFile = document.querySelector('#uploadFile')
+document.querySelector('#buttonText').innerHTML = 'Bitte beachte, dass alle mit * markierten Felder Pflichtfelder sind!';
+
+uploadFile.addEventListener('keyup', function (e) {
+    const requiredInputs = document.querySelectorAll('input[required]')
+    const submitBtn = document.querySelector('#submitBtn')
+    let buttonDisabled = false
+    requiredInputs.forEach(function (input) {
+        if (input.value === '' || !input.value.replace(/\s/g, '').length) {
+            buttonDisabled = true
+        }
+    })
+    if (buttonDisabled) {
+        document.querySelector('#buttonText').innerHTML = 'Bitte beachte, dass alle mit * markierten Felder Pflichtfelder sind!';
+        submitBtn.setAttribute('disabled', 'disabled')
+    } else {
+        document.querySelector('#buttonText').innerHTML = '';
+        submitBtn.removeAttribute('disabled')
+    }
+})
+
+// const uploadFile = document.querySelector('#uploadFile')
+// document.querySelector('#buttonText').innerHTML = 'Bitte beachte, dass alle mit * markierten Felder Pflichtfelder sind!';
+
+// uploadFile.addEventListener('keyup', function (e) {
+//     // eslint-disable-next-line no-undef
+//     $('.requiredFields').each(function () {
+//         let buttonDisabled = true;
+//         const componentName = document.querySelector('#componentName').value;
+//         const inputVersion = document.querySelector('#inputVersion').value;
+//         const value = this.value;
+//         if (value && (value.trim() !== '') && componentName && inputVersion) {
+//             console.log('IF CASE');
+//             buttonDisabled = false;
+//             // eslint-disable-next-line no-undef
+//             $('#submitBtn').attr('disabled', false);
+//             document.querySelector('#buttonText').innerHTML = ''
+//         } else {
+//             console.log('ELSE CASE');
+//             // eslint-disable-next-line no-unused-vars
+//             buttonDisabled = true;
+//             // eslint-disable-next-line no-undef
+//             $('#submitBtn').attr('disabled', true);
+//             document.querySelector('#buttonText').innerHTML = 'Bitte beachte, dass alle mit * markierten Felder Pflichtfelder sind!';
+//         }
+//     });
+// })
+
+// // File upload
+// const inputs = document.querySelectorAll('.requiredFields');
+// console.log('inputs', inputs);
+// const button = document.querySelector('#submitBtn');
+// button.disabled = true;
+// document.querySelector('#buttonText').innerHTML = 'Bitte beachte, dass alle mit * markierten Felder Pflichtfelder sind!';
+// inputs.addEventListener('change', stateHandle);
+
+// // eslint-disable-next-line no-unused-vars
+// function stateHandle () {
+//     if (document.querySelector('#componentName').value === '') {
+//         button.disabled = true;
+//         document.querySelector('#buttonText').innerHTML = 'Bitte beachte, dass alle mit * markierten Felder Pflichtfelder sind!';
+//     } else {
+//         button.disabled = false;
+//         document.querySelector('#buttonText').innerHTML = '';
+//     }
+// }
