2787c3624300081f440bcce74491b272224291e2
Nisha Shukla <nisha.shukla@inspired.consulting>
2023-02-02 15:34:19 +0100

    included homepage animation, routes and methods for update components

--- component_store/src/routes/fileUpload.js
+++ component_store/src/routes/fileUpload.js
@@ -1,14 +1,66 @@
 const express = require('express');
 const fs = require('fs');
-const { addToDB } = require('../model/componentTest');
-// const component = require('../models/component');
+const { addToDB } = require('../models/database');
 const path = require('path');
 const router = express.Router();
+const component = require('../models/component');
 
 router.get('/', (req, res) => {
     res.render('fileupload', {
         url: '/upload'
     })
+});
+
+router.get('/update', (req, res) => {
+    const componentName = req.query.componentName
+
+    component.findComponents('name', componentName)
+        .then((rows) => {
+            // console.log('rows from update##', rows);
+            res.render('updateComponent', {
+                url: '/upload/update',
+                componentName
+            })
+        })
+});
+
+router.post('/update/:componentName', (req, res, next) => {
+    const sampleFile = req.files.updateFileUpload;
+    const componentName = req.params.componentName;
+    const filename = sampleFile.name;
+    const data = req.body;
+    const inputVersion = data.updateInputVersion;
+
+    writeFileLocally(filename)
+        .then(result => {
+            addToDB(data, componentName).then(() => {
+                fs.mkdir(`./uploads/${componentName}/${inputVersion}`, { recursive: true }, (err) => {
+                    if (err) throw err;
+                    fs.readdir(`./uploads/${componentName}/${inputVersion}`, (err, files) => {
+                        if (err) throw err;
+                        if (files.length > 0) {
+                            for (const file of files) {
+                                fs.unlink(path.join(`./uploads/${componentName}/${inputVersion}`, file), (err) => {
+                                    if (err) throw err;
+                                });
+                            }
+                        }
+                    });
+                    sampleFile.mv(`./uploads/${componentName}/${inputVersion}/` + filename, function (err) {
+                        if (err) return res.status(500).send(err);
+                        return res.redirect(`/componentDetails/${componentName}`);
+                    });
+                })
+            })
+                .catch(err => {
+                    if (err) return res.status(500).send('addToDB throws error');
+                    return next(err);
+                })
+        })
+        .catch(err => {
+            if (err) return res.status(500).send(err);
+            return next(err);
+        })
 });
 
 router.post('/', (req, res, next) => {
