e14256afefcce55350384176ba3cf736c3e7287a
Nisha Shukla <nisha.shukla@inspired.consulting>
2023-02-10 16:40:40 +0100

    updated config with envrc variables and used it in pgpool js

--- component_store/config/config.js
+++ component_store/config/config.js
@@ -1,6 +1,11 @@
 
 exports.load = () => {
     return {
-        COMPONENTS_LIMIT_PER_PAGE: 6
+        COMPONENTS_LIMIT_PER_PAGE: 6,
+        DB_HOST: process.env.DB_HOST,
+        DB_PORT: process.env.DB_PORT,
+        DB_DATABASE: process.env.DB_DATABASE,
+        DB_USER: process.env.DB_USER,
+        DB_PASS: process.env.DB_PASS
     }
 }
--- component_store/src/helpers/pgpool.js
+++ component_store/src/helpers/pgpool.js
@@ -1,12 +1,14 @@
 
 const { Pool } = require('pg');
+const Configuration = require('../../config/config')
+const config = Configuration.load();
 
 const poolConfig = {
-    host: '127.0.0.1',
-    port: 25443,
-    user: 'postgres',
-    password: 'password',
-    database: 'componentstore'
+    host: config.DB_HOST,
+    port: config.DB_PORT,
+    user: config.DB_USER,
+    password: config.DB_PASS,
+    database: config.DB_DATABASE
 };
 
 let pool;
