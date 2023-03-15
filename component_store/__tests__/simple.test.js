1eefd2c6b8203e67a4d9d8059b7e5d11e598442c
Nisha Shukla <nisha.shukla@inspired.consulting>
2023-03-15 13:03:25 +0100

    added jest

---
+++ component_store/__tests__/simple.test.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+test('1 + 1 = 2', () => {
+    // eslint-disable-next-line no-undef
+    expect(1 + 1).toBe(2);
+});
+
+// eslint-disable-next-line no-undef
+test('1 + 2 = 3', () => {
+    // eslint-disable-next-line no-undef
+    expect(1 + 2).toBe(3);
+});
