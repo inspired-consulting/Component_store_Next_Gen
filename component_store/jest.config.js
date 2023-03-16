ad5607ed5beb8ad52b6ec47a2d5aca87cc7bc44d
Nisha Shukla <nisha.shukla@inspired.consulting>
2023-03-16 16:46:55 +0100

    added new config.yml

---
+++ component_store/jest.config.js
@@ -0,0 +1,7 @@
+
+const config = {
+    globalSetup: '<rootDir>/node_modules/@databases/pg-test/jest/globalSetup',
+    globalTeardown: '<rootDir>/node_modules/@databases/pg-test/jest/globalTeardown'
+};
+
+module.exports = config;
